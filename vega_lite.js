59a7ff8b66888a87c929ae68a970cd4adffdfe37
Ochir-Erdene Purevdavaa <opur0002@student.monash.edu>
2025-10-17 03:32:59 +1100

    index + js

---
+++ vega_lite.js
@@ -0,0 +1,6 @@
+var vg_1 = "Graph1.json";
+vegaEmbed("#map1", vg_1).then(function(result) {
+// Access the Vega view instance 
+// (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
