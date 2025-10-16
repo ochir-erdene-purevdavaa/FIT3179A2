71f89f69df7911435cc0068f0f20ea589cc247c2
Ochir-Erdene Purevdavaa <opur0002@student.monash.edu>
2025-10-17 10:49:32 +1100

    Updated index with 2 map graphs, 1 imconplete

--- vega_lite.js
+++ vega_lite.js
@@ -1,6 +1,17 @@
 var vg_1 = "Graph1.json";
-vegaEmbed("#map1", vg_1).then(function(result) {
+var vg_2 = "Graph2.json";
+var vg_3 = "Graph3.json";
+
+vegaEmbed("#map1", vg_1, { actions: false }).then(function(result) {
 // Access the Vega view instance 
 // (https://vega.github.io/vega/docs/api/view/) as result.view
 
+}).catch(console.error);
+
+// Map 2
+vegaEmbed("#map2", vg_2, { actions: false }).then(function(result) {
+}).catch(console.error);
+
+// Map 3
+vegaEmbed("#map3", vg_3, { actions: false }).then(function(result) {
 }).catch(console.error);
