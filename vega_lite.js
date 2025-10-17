8f7a53ce7acd3c0608e21184a608291294adf5d8
Ochir-Erdene Purevdavaa <opur0002@student.monash.edu>
2025-10-17 12:05:26 +1100

    initial bar graphs

--- vega_lite.js
+++ vega_lite.js
@@ -1,6 +1,8 @@
 var vg_1 = "Graph1.json";
 var vg_2 = "Graph2.json";
 var vg_3 = "Graph3.json";
+var bg1 = "barGraph1.json";
+var bg2 = "barGraph2.json";
 
 vegaEmbed("#map1", vg_1, { actions: false }).then(function(result) {
 // Access the Vega view instance 
@@ -15,3 +17,10 @@ vegaEmbed("#map2", vg_2, { actions: false }).then(function(result) {
 // Map 3
 vegaEmbed("#map3", vg_3, { actions: false }).then(function(result) {
 }).catch(console.error);
+
+vegaEmbed("#bg1", bg1, { actions: false }).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#bg2", bg2, { actions: false }).then(function(result) {
+}).catch(console.error);
+
