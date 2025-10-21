02c65dd1195c71c36a730aae1be62fe2d8895acc
Ochir-Erdene Purevdavaa <opur0002@student.monash.edu>
2025-10-21 16:40:16 +1100

    updated index page + style

--- vega_lite.js
+++ vega_lite.js
@@ -3,6 +3,7 @@ var vg_2 = "Graph2.json";
 var vg_3 = "Graph3.json";
 var bg1 = "barGraph1.json";
 var bg2 = "barGraph2.json";
+var hmap1 = "heatmap1.json";
 
 vegaEmbed("#map1", vg_1, { actions: false }).then(function(result) {
 // Access the Vega view instance 
@@ -18,9 +19,14 @@ vegaEmbed("#map2", vg_2, { actions: false }).then(function(result) {
 vegaEmbed("#map3", vg_3, { actions: false }).then(function(result) {
 }).catch(console.error);
 
+//Bar graphs
 vegaEmbed("#bg1", bg1, { actions: false }).then(function(result) {
 }).catch(console.error);
 
 vegaEmbed("#bg2", bg2, { actions: false }).then(function(result) {
+}).catch(console.error);
+
+//heatmap
+vegaEmbed("#hmap1", hmap1, { actions: false }).then(function(result) {
 }).catch(console.error);
 
